379c4cb826ddf1d426bcaf7efacf9fbfede7cf75
samBiotic <samBiotic@gmail.com>
2010-12-15 14:56:17 +0000

    looped the browser thread

--- zosc.rb
+++ zosc.rb
@@ -11,29 +11,34 @@ zeroconf_registrar = Thread.new() {
   end
 }
 
-zeroconf_browse_and_resolve = Thread.new() {
-  browser = DNSSD::Service.new
-  services = {}
+zeroconf_loop = Thread.new() {
+  while (1)
+    zeroconf_browse_and_resolve = Thread.new() {
+      browser = DNSSD::Service.new
+      services = {}
 
-  browser.browse '_osc._udp' do |reply|
-    services[reply.fullname] = reply
-    next if reply.flags.more_coming?
+      browser.browse '_osc._udp' do |reply|
+        services[reply.fullname] = reply
+        next if reply.flags.more_coming?
 
-    services.sort_by do |_, service|
-      [(service.flags.add? ? 0 : 1), service.fullname]
-    end.each do |_, service|
-      next unless service.flags.add?
+        services.sort_by do |_, service|
+          [(service.flags.add? ? 0 : 1), service.fullname]
+        end.each do |_, service|
+          next unless service.flags.add?
 
-      resolver = DNSSD::Service.new
-      resolver.resolve service do |r|
-        puts "#{r.name} on #{r.target}:#{r.port}"
-        break unless r.flags.more_coming?
+          resolver = DNSSD::Service.new
+          resolver.resolve service do |r|
+            puts "#{r.name} on #{r.target}:#{r.port}"
+            break unless r.flags.more_coming?
+          end
+
+          resolver.stop
+        end
+
+        services.clear
       end
-
-      resolver.stop
-    end
-
-    services.clear
+    }
+    sleep(5)
   end
 }
 
